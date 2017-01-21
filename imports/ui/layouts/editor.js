91ae9a4e8b3666043279eee3bb8d0e417ead3db9
Jeremy Iglehart <jeremy@autoschematic.com>
2017-01-21 06:24:29 -0500

    Loading State from raw HTML

--- imports/ui/layouts/editor.js
+++ imports/ui/layouts/editor.js
@@ -1,21 +1,108 @@
 import React, { Component } from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
-import {Editor, EditorState} from 'draft-js';
+import {
+          CompositeDecorator,
+          ContentBlock,
+          ContentState,
+          Editor,
+          EditorState,
+          Entity,
+          convertFromHTML,
+          convertToRaw,
+          RichUtils
+        } from 'draft-js'
+
+import RaisedButton from 'material-ui/RaisedButton'
+import FontIcon from 'material-ui/FontIcon'
+import IconButton from 'material-ui/IconButton'
+
+const iconStyles = {
+}
+
+const buttonStyle = {
+  margin: 12,
+}
 
 class MyEditor extends Component {
   constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
+    super(props)
+
+    const decorator = new CompositeDecorator([
+      {
+        strategy: findLinkEntities,
+        component: Link,
+      }
+    ])
+
+    const sampleMarkup =
+      '<b>Bold text</b>, <i>Italic text</i><br/ ><br />' +
+      '<a href="http://www.facebook.com">Example link</a>'
+
+    const blocksFromHTML = convertFromHTML(sampleMarkup)
+    const state = ContentState.createFromBlockArray(blocksFromHTML)
+
+    this.state = {
+      editorState: EditorState.createWithContent(
+        state,
+        decorator,
+      ),
+    }
+    this.onChange = (editorState) => this.setState({editorState})
+  }
+  logState() {
+    const content = this.state.editorState.getCurrentContent()
+    console.log(convertToRaw(content))
+  }
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
+    if (newState) {
+      this.onChange(newState)
+      return 'handled'
+    }
+    return 'not-handled'
+  }
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
   }
   render() {
     return (
       <div className="editor-container">
         <h1>DraftJS and Meteor Editor:</h1>
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        <IconButton onClick={this._onBoldClick.bind(this)} touch={true} tooltip="Format Bold" tooltipPosition="top-right">
+          <FontIcon className="material-icons" style={iconStyles}>format_bold</FontIcon>
+        </IconButton>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          handleKeyCommand={this.handleKeyCommand.bind(this)}
+          />
+        <RaisedButton label="Log Editor State to Console" onClick={this.logState.bind(this)} primary={true} style={buttonStyle} />
       </div>
-    );
+    )
   }
+}
+
+
+function findLinkEntities(contentBlock, callback) {
+  contentBlock.findEntityRanges(
+    (character) => {
+      const entityKey = character.getEntity()
+      return (
+        entityKey !== null &&
+        Entity.get(entityKey).getType() === 'LINK'
+      )
+    },
+    callback
+  )
+}
+
+const Link = (props) => {
+  const {url} = Entity.get(props.entityKey).getData()
+  return (
+    <a href={url}>
+      {props.children}
+    </a>
+  )
 }
 
 export default createContainer(() => {
--- imports/ui/pages/app.js
+++ imports/ui/pages/app.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 export default (props) => {
   return (
     <div className="draft-js-meteor-app">
-      {props.children}
+      <MuiThemeProvider>
+        {props.children}
+      </MuiThemeProvider>
     </div>
   )
 }
